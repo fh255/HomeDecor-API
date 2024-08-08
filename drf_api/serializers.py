a3692ccd988129f2aa61dd877fb8b25016df6695
Fatema Mahabub <fmahabub8@gmail.com>
2024-08-08 18:42:27 +0000

    changed image serializable

--- drf_api/serializers.py
+++ drf_api/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 
 class CurrentUserSerializer(UserDetailsSerializer):
     profile_id = serializers.ReadOnlyField(source='profile.id')
-    profile_image = serializers.SerializerMethodField()
+    profile_image = serializers.ReadOnlyField(source='profile.image.url')
 
     def to_representation(self, instance):
         # Call the parent's to_representation method to get the serialized data

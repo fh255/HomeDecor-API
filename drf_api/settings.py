bb0446c2edb21802a98e5553e28a0d86889d67a3
Fatema Mahabub <fmahabub8@gmail.com>
2024-08-18 10:04:19 +0000

    update the settings

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -105,14 +105,14 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [os.environ.get("CLIENT_ORIGIN")]
 
-if 'CLIENT_ORIGIN_DEV' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN_DEV')
-    ]
-else:
-     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod$",
-    ]
+# if 'CLIENT_ORIGIN_DEV' in os.environ:
+#     CORS_ALLOWED_ORIGINS = [
+#         os.environ.get('CLIENT_ORIGIN_DEV')
+#     ]
+# else:
+#      CORS_ALLOWED_ORIGIN_REGEXES = [
+#         r"^https://.*\.gitpod$",
+#     ]
 
 CORS_ALLOW_CREDENTIALS = True
 
